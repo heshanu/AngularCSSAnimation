a52e05f4fb8ad4c4d834b9c2d669c3a92f5691e3
heshanu <heshanu97@gmail.com>
2022-04-22 12:04:09 -0700

    added

--- css-animations-finished/css-animations-finished/src/app/app.component.ts
+++ css-animations-finished/css-animations-finished/src/app/app.component.ts
@@ -6,8 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  //title = 'AngularCssAnimation';
-  divClicked=false;
-
-  
+  divClicked = false;
+  doAnimate = false;
 }
